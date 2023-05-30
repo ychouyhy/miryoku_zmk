e2df8062073b171043f6cf2b0074736b5f209d47
ychouyhy <134882849+ychouyhy@users.noreply.github.com>
2023-05-29 17:23:50 -0700

    Update kyria.h
    This will vanish on pull

--- miryoku/mapping/50/kyria.h
+++ miryoku/mapping/50/kyria.h
@@ -15,7 +15,7 @@
 XXX  K00  K01  K02  K03  K04                           K05  K06  K07  K08  K09  XXX  \
 XXX  K10  K11  K12  K13  K14                           K15  K16  K17  K18  K19  XXX  \
 XXX  K20  K21  K22  K23  K24  XXX  XXX       XXX  XXX  K25  K26  K27  K28  K29  XXX  \
-               XXX  XXX  K32  K33  K34       K35  K36  K37  XXX  XXX
+               XXX  XXX  K32  K33  K34       K36  K35  K37  XXX  XXX
 #define MIRYOKU_KLUDGE_THUMBCOMBOS_LEFT 43 44
 #define MIRYOKU_KLUDGE_THUMBCOMBOS_RIGHT 45 46
 #else
