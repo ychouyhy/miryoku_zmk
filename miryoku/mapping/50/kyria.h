33546a1e2de9142a546e95704d5eb917697d90e2
ychouyhy <134882849+ychouyhy@users.noreply.github.com>
2023-05-29 18:21:31 -0700

    Update kyria.h

--- miryoku/mapping/50/kyria.h
+++ miryoku/mapping/50/kyria.h
@@ -12,9 +12,9 @@
      K20, K21, K22, K23, K24,                          K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,                          K35, K36, K37, N38, N39 \
 )\
-XXX  K00  K01  K02  K03  K04                           K05  K06  K07  K08  K09  XXX  \
-XXX  K10  K11  K12  K13  K14                           K15  K16  K17  K18  K19  XXX  \
-XXX  K20  K21  K22  K23  K24  XXX  XXX       XXX  XXX  K25  K26  K27  K28  K29  XXX  \
+&kp TAB  K00  K01  K02  K03  K04                           K05  K06  K07  K08  K09  &kp MINUS  \
+&kp ESC  K10  K11  K12  K13  K14                           K15  K16  K17  K18  K19  &kp SEMI  \
+&kp LPAR  K20  K21  K22  K23  K24  XXX  XXX       XXX  XXX  K25  K26  K27  K28  K29 &kp LBKT \
                XXX  XXX  K32  K33  K34       K36  K35  K37  XXX  XXX
 #define MIRYOKU_KLUDGE_THUMBCOMBOS_LEFT 43 44
 #define MIRYOKU_KLUDGE_THUMBCOMBOS_RIGHT 45 46
